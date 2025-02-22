961239feca657ac553545dd953fa35980d3a92c4
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-22 10:00:32 +0200

    Modifying MealView

--- app/src/main/java/com/example/tabty/Meal/view/MealView.java
+++ app/src/main/java/com/example/tabty/Meal/view/MealView.java
@@ -1,4 +1,11 @@
 package com.example.tabty.Meal.view;
 
+import com.example.tabty.Model.DB.Meal;
+import com.example.tabty.Model.DB.MealEntity;
+import com.example.tabty.Model.Network.POJOs.Ingredient;
+
+import java.util.List;
+
 public interface MealView {
+    public void onFavouriteButtonClicked(MealEntity meal);
 }
