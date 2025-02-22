d2e6b7b155b215ee5f1553046fd0a005f7dcb184
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-22 09:57:25 +0200

    Modifying MealPresenter.java

--- app/src/main/java/com/example/tabty/Meal/presenter/MealPresenter.java
+++ app/src/main/java/com/example/tabty/Meal/presenter/MealPresenter.java
@@ -1,14 +1,32 @@
 package com.example.tabty.Meal.presenter;
 
 import com.example.tabty.Meal.view.MealView;
+import com.example.tabty.Model.DB.Meal;
+import com.example.tabty.Model.DB.MealEntity;
 import com.example.tabty.Model.MealsRepository;
+
+import kotlin.text.MatchResult;
+import kotlin.text.Regex;
 
 public class MealPresenter {
     MealsRepository myRepo;
-    MealView myView;
-
-   public MealPresenter(MealsRepository myRepo , MealView myView){
+   public MealPresenter(MealsRepository myRepo){
         this.myRepo=myRepo;
-        this.myView=myView;
     }
+    public void insertLocalMeal(MealEntity meal){
+       myRepo.insertLocalMeal(meal);
+    }
+    public void insertMealIntoCalendar(MealEntity meal){
+       //To-Do
+    }
+
+    public String getVideoKey(String url){
+        MatchResult matchResult = new Regex("v=([^&]+)").find(url, 0);
+        if (matchResult != null) {
+            return matchResult.getGroups().get(1).getValue();
+        } else {
+            return null;
+        }
+    }
+
 }
