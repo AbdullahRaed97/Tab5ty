04489f927130a2599c20f5537a043dcbe15570a9
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-22 09:56:27 +0200

    adding YoutubePlayerView and handle its logic

--- app/src/main/java/com/example/tabty/Meal/view/MealFragment.java
+++ app/src/main/java/com/example/tabty/Meal/view/MealFragment.java
@@ -1,22 +1,46 @@
 package com.example.tabty.Meal.view;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.tabty.Meal.presenter.MealPresenter;
+import com.example.tabty.Model.DB.Meal;
+import com.example.tabty.Model.DB.MealEntity;
+import com.example.tabty.Model.DB.MealsLocalDataSource;
+import com.example.tabty.Model.MealsRepository;
+import com.example.tabty.Model.Network.MealRemoteDataSource;
+import com.example.tabty.Model.Network.POJOs.Ingredient;
 import com.example.tabty.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import java.util.List;
 
 
-public class MealFragment extends Fragment implements MealView{
+public class MealFragment extends Fragment {
 ImageView mealImage;
 ImageButton favBtn;
 ImageButton calendarBtn;
@@ -24,7 +48,13 @@ TextView mealName;
 TextView mealType;
 ImageView countryFlag;
 TextView instText;
-WebView webView;
+View myView;
+RecyclerView recyclerView;
+IngredientsAdapter myAdapter;
+YouTubePlayerView youtubeView;
+MealPresenter presenter;
+MealEntity myMeal;
+    private static final String TAG = "MealFragment";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +77,42 @@ WebView webView;
         mealType=view.findViewById(R.id.mealType_tv);
         countryFlag=view.findViewById(R.id.countryFlag_iv);
         instText=view.findViewById(R.id.instructionsText);
-        webView=view.findViewById(R.id.webView);
+        recyclerView=view.findViewById(R.id.ingredientRecyclerView);
+        youtubeView=view.findViewById(R.id.youtubeView);
+        myView=view;
+        MealsRepository myRepo = MealsRepository.getInstance(MealRemoteDataSource.getInstance(),new MealsLocalDataSource(getContext()));
+        presenter = new MealPresenter(myRepo);
+
+        //receive clicked meal from home
+        Meal recievedMeal = MealFragmentArgs.fromBundle(getArguments()).getMeal();
+        myMeal=new MealEntity(recievedMeal);
+        mealName.setText(recievedMeal.getStrMeal());
+        instText.setText(recievedMeal.getStrInstructions());
+        Glide.with(getContext()).load(recievedMeal.getStrMealThumb())
+                .apply(new RequestOptions().override(500,500)).into(mealImage);
+        mealType.setText(recievedMeal.getStrArea());
+        getLifecycle().addObserver(youtubeView);
+
+        //Load The video
+        youtubeView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                super.onReady(youTubePlayer);
+                String videoKey = presenter.getVideoKey(recievedMeal.getStrYoutube());
+                Log.i(TAG, "onReady: "+videoKey);
+                youTubePlayer.loadVideo(videoKey,0);
+            }
+        });
+
+        favBtn.setOnClickListener(v->{
+            Snackbar.make(myView,"Meal is Added to Favourite",Snackbar.LENGTH_SHORT).show();
+            presenter.insertLocalMeal(myMeal);
+            Log.i(TAG, "onViewCreated: "+myMeal);
+        });
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        layoutManager.setOrientation(GridLayoutManager.HORIZONTAL);
+        myAdapter=new IngredientsAdapter(getContext(),recievedMeal.getIngredients(),recievedMeal.getMeasures());
+        recyclerView.setAdapter(myAdapter);
     }
 }
