a90d817f99fe7c88bf179efae215d9e920ab2f9b
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-24 00:21:40 +0200

    add OnCalendarDeleteClickListener methods

---
+++ app/src/main/java/com/example/tabty/calendar/view/OnCalendarDeleteClickListener.java
@@ -0,0 +1,7 @@
+package com.example.tabty.calendar.view;
+
+import com.example.tabty.model.db.PlannedMeal;
+
+public interface OnCalendarDeleteClickListener {
+    public void onDeleteClickAction(PlannedMeal meal);
+}
