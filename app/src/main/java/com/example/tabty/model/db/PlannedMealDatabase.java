261000f5cefdba890f74c97a6f7777defdeccc71
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-24 00:22:58 +0200

    .

--- app/src/main/java/com/example/tabty/model/db/PlannedMealDatabase.java
+++ app/src/main/java/com/example/tabty/model/db/PlannedMealDatabase.java
@@ -2,13 +2,18 @@ package com.example.tabty.model.db;
 
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
 
+import com.example.tabty.utilities.DatabaseConverters;
+
+@Database(entities = {PlannedMeal.class}, version = 1)
+@TypeConverters({DatabaseConverters.class})
 public abstract  class PlannedMealDatabase extends RoomDatabase {
-
     private static PlannedMealDatabase instance = null;
-
     public abstract PlannedMealDao getPlannedMealDao();
 
     public static PlannedMealDatabase getInstance(Context context) {
