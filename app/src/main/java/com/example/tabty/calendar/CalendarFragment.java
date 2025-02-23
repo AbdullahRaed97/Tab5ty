30cc0c394d2d1fef549e8e6d8b8448036baafa49
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-23 21:12:58 +0200

    .

--- app/src/main/java/com/example/tabty/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/tabty/calendar/CalendarFragment.java
@@ -1,4 +1,4 @@
-package com.example.tabty;
+package com.example.tabty.calendar;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.tabty.R;
 
 /**
  * A simple {@link Fragment} subclass.
