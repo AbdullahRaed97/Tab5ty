fcabb6306203f62fb4e81f7c7831ed503cbfdf46
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-23 17:19:18 +0200

    .

--- app/src/main/java/com/example/tabty/utilities/FirebaseCallback.java
+++ app/src/main/java/com/example/tabty/utilities/FirebaseCallback.java
@@ -1,4 +1,4 @@
-package com.example.tabty.Utilities;
+package com.example.tabty.utilities;
 
 public interface FirebaseCallback {
     public void onFirebaseResponse(boolean success,String errorMessage);
