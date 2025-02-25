0ffbf473fb425db56af4c51799666c785915bf22
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-25 17:00:49 +0200

    modify FavouritePresenter based on Rxjava

--- app/src/main/java/com/example/tabty/favourite/presenter/FavouritePresenter.java
+++ app/src/main/java/com/example/tabty/favourite/presenter/FavouritePresenter.java
@@ -1,17 +1,13 @@
 package com.example.tabty.favourite.presenter;
 
-import androidx.lifecycle.LiveData;
+import android.annotation.SuppressLint;
 
 import com.example.tabty.favourite.view.FavouriteView;
 import com.example.tabty.model.MealsRepository;
 import com.example.tabty.model.db.MealEntity;
-
-import java.util.List;
-
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.CompletableObserver;
-import io.reactivex.rxjava3.core.SingleObserver;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -24,7 +20,10 @@ public class FavouritePresenter {
         this.myView=myView;
     }
     public void deleteLocalMeal(MealEntity meal){
-        myRepo.deleteLocalMeal(meal).subscribe(new CompletableObserver() {
+        myRepo.deleteLocalMeal(meal)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new CompletableObserver() {
             @Override
             public void onSubscribe(@NonNull Disposable d) {
                 //do-nothing
@@ -41,24 +40,13 @@ public class FavouritePresenter {
             }
         });
     }
+    @SuppressLint("CheckResult")
     public void getAllLocalMeals(){
-         myRepo.getAllLocalMeals().subscribeOn(Schedulers.io())
+         myRepo.getAllLocalMeals()
+                 .subscribeOn(Schedulers.io())
                  .observeOn(AndroidSchedulers.mainThread())
-                 .subscribe(new SingleObserver<List<MealEntity>>() {
-                     @Override
-                     public void onSubscribe(@NonNull Disposable d) {
-                         //Do-Nothing
-                     }
-
-                     @Override
-                     public void onSuccess(@NonNull List<MealEntity> mealEntities) {
-                         myView.onFavouriteMealListSuccess(mealEntities);
-                     }
-
-                     @Override
-                     public void onError(@NonNull Throwable e) {
-                        myView.onFavouriteMealListFailure(e.getLocalizedMessage());
-                     }
-                 });
+                 .subscribe(
+                         item ->  myView.onFavouriteMealListSuccess(item),
+                         error ->myView.onFavouriteMealListFailure(error.getLocalizedMessage()));
     }
 }
