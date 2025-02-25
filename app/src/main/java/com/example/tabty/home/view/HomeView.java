c522c1b25330b319398dd408f5276ff74118de08
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-25 17:04:16 +0200

    modify home view

--- app/src/main/java/com/example/tabty/home/view/HomeView.java
+++ app/src/main/java/com/example/tabty/home/view/HomeView.java
@@ -7,6 +7,6 @@ import java.util.List;
 public interface HomeView {
     public void showAllMealsByFirstLetter(List<Meal> meals);
     public void showAllMealsByFirstLetterError(String errorMessage);
-    public void showRandomMeal(List<Meal> meals);
+    public void showRandomMeal(Meal meal);
     public void showRandomMealError(String errorMessage);
 }
