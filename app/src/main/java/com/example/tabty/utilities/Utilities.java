ac5cbf3115595d12b3ac693a1b1eb4c33c06df93
Abdullah Raed <abdullahraediti@gmail.com>
2025-02-27 21:55:47 +0200

    add Utilities.java class

---
+++ app/src/main/java/com/example/tabty/utilities/Utilities.java
@@ -0,0 +1,41 @@
+package com.example.tabty.utilities;
+
+public class Utilities {
+    public static String getCountryNameCode(String countryName) {
+        countryName = countryName.toLowerCase();
+
+        switch (countryName) {
+            case "american": return "us";
+            case "british": return "gb";
+            case "canadian": return "ca";
+            case "chinese": return "cn";
+            case "croatian": return "hr";
+            case "dutch": return "nl";
+            case "egyptian": return "eg";
+            case "french": return "fr";
+            case "greek": return "gr";
+            case "indian": return "in";
+            case "irish": return "ie";
+            case "italian": return "it";
+            case "jamaican": return "jm";
+            case "japanese": return "jp";
+            case "kenyan": return "ke";
+            case "malaysian": return "my";
+            case "mexican": return "mx";
+            case "moroccan": return "ma";
+            case "polish": return "pl";
+            case "portuguese": return "pt";
+            case "russian": return "ru";
+            case "spanish": return "es";
+            case "thai": return "th";
+            case "tunisian": return "tn";
+            case "turkish": return "tr";
+            case "vietnamese": return "vn";
+            case "filipino": return "ph";
+            case "ukrainian": return "ua";
+            case "uruguayan": return "uy";
+            case "norwegian": return "no";
+            default: return countryName;
+        }
+    }
+}
